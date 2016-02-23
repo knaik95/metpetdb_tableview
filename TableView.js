d82012d3283da5f18c704db9ceabf97f44dd36e4
knaik95 <knaik95@gmail.com>
2016-02-23 13:18:47 -0500

    javascript and html integration

--- TableView.js
+++ TableView.js
@@ -1,14 +1,35 @@
-<script>
-	var a = {};
-	$.getJSON('file.json', function(data){
-		a = data;
-		$.each(a, function(idx, rock){
-			$('table#t TBODY').append('<tr><td>'+rock.SampleNumber+'</td><td>'+rock.RockType +'</td><td>'+rock.Latitude +'</td></tr>'+rock.Longitude +'</td></tr>');
-		});
-	});
-</script>
+$.ajax({
+    url: '/echo/json/',
+    type: "post",
+    dataType: "json",
+    data: {
+        json: JSON.stringify([
+            {
+            id: 1,
+            elementName: "Germanium",
+            atomicNumber: "13"},
+        {
+            id: 2,
+            elementName: "Uranium",
+            atomicNumber: "92"}
+        ]),
+        delay: 3
+    },
+    success: function(data, textStatus, jqXHR){
+        // since we are using jQuery, you don't need to parse response
+        drawTable(data);
+    }
+});
 
-<table id="t">
-	<thread><tr><th>SampleNumber<\th><th>RockType<\th><th>Latitude<\th><th>Longitude<\th>
-	<tbody></tbody>
-</table>
+function drawTable(data){
+    for(var i = 0; i < data.length; i++)
+        drawRow(data[i]);
+}
+
+function drawRow(rowData){
+    var row = $("<tr />")
+    $("#personDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData.id + "</td>"));
+    row.append($("<td>" + rowData.rockType + "</td>"));
+    row.append($("<td>" + rowData.atomicNumber + "</td>"));
+}
